9af3cdd79883c6d98df011d464ff05bb3beb45ca
KopIAkash1 <andreymart01@gmail.com>
2025-03-02 15:12:28 +0300

    assigneeAPI: spam logic added

--- assigneeAPI.py
+++ assigneeAPI.py
@@ -55,7 +55,6 @@ def fromate_to_ticket(response):
     return tickets
 
 def get_tickets(name):
-    #url_test = "https://tracker.ntechlab.com/api/issues?fields=id,idReadable,summary,description&query=Assignee: v.golovenko@ntechlab.com State: -Closed"
     url = f'https://tracker.ntechlab.com/api/issues?fields=id,idReadable,summary,description&query=Assignee: {name} State: -Closed'
     print(f"[DEBUG] making request to get tickets: {url}")
     url_headers = {
@@ -101,6 +100,44 @@ def assigne_to_next(old_user_param : str = "",next_user_param : str = "") -> str
     #send_assigne_to_request(tickets, next_user)
     return config.user_tg[next_user]
 
+def send_change_request_ticket(ticket_id, data, field):
+    url = f"https://tracker.ntechlab.com/api/issues/{ticket_id}/fields/{field}"
+    url_headers = {
+        'Accept': 'application/json',
+        f'Authorization': f'Bearer {config.token}',
+        'Content-Type': 'application/json'
+    }
+    response = requests.post(url, json=data, headers=url_headers, verify=False)
+    return response.status_code, response.text
+
+def spam_ticket(ticket_id):
+    current_user, _ = read_schedule()
+
+    data = { "id": "159-2506", "value": {"ringId": f"{config.user_ring_id[current_user]}" } }
+    status, text = send_change_request_ticket(ticket_id, data, "159-2506")
+    print(f"DEBUG | Send request to assignee on ticket: {ticket_id} | {status, text}")
+
+    data = {"id": "158-10832","value": {"description": f"","id": "138-5442", "kind": "enum","label": "Спам","name": "Спам"}}
+    status, text = send_change_request_ticket(ticket_id, data, "158-10832")
+    print(f"DEBUG | Send request to customer on ticket: {ticket_id} | {status, text}")
+    
+    data = {"id": "517-16752","value": f"Спам"}
+    status, text = send_change_request_ticket(ticket_id, data, "517-16752")
+    print(f"DEBUG | Send request to solution on ticket: {ticket_id} | {status, text}")
+
+    data = {"id": "158-10166","value": [{"description": None,"id": "166-796","kind": "enum","label": "Spam","name": "Spam"}]}
+    status, text = send_change_request_ticket(ticket_id, data, "158-10166")
+    print(f"DEBUG | Send request to subsystem on ticket: {ticket_id} | {status, text}")
+
+    data = {"id": "158-10165","event": {"id": f"{"resolved"}"}}
+    status, text = send_change_request_ticket(ticket_id, data, "158-10165")
+    print(f"DEBUG | Send request to state on ticket: {ticket_id} | {status, text}")
+
+    data = {"id": "158-10165","event": {"id": f"{"closed"}"}}
+    status, text = send_change_request_ticket(ticket_id, data, "158-10165")
+    print(f"DEBUG | Send request to state on ticket: {ticket_id} | {status, text}")
+
+    print("END OF SENDING REQUESTS")
 
 if __name__ == "__main__":
     assigne_to_next()
