4461d9b27f29cce9f0c4c9ba9298e4592ff89b78
Shiretomi <Shiretomi@gmail.com>
2025-03-19 02:40:03 +0300

    basic functionality for working with db

--- utils/db.py
+++ utils/db.py
@@ -3,21 +3,34 @@ import sqlite3
 db = sqlite3.connect("allowed_to_tickets.db", check_same_thread=False)
 cursor = db.cursor()
 
-def add_tickets_to_user(user, tickets):
-    if not __is_user_exist(user): __add_user(user, tickets)
+#Функцию вызывать с параметрами add_tickets_to_user(user, tickets="")
+def set_tickets_to_user(user, tickets):
+    if not __is_user_exist(user): __set_user(user, tickets)
+    __set_tickets(user, tickets)
     
-#TODO: Доделать вытаскивание тикетов для дальнейшей проверки (можно возвращать текстом)
+#Возвращает все тикеты пользователя как строка, обрабатываем там, откуда вызывали
+#Если вызываем и выдает None -> return "Пользователь не найден" или чонить подобное
 def get_tickets_by_user(user) -> str:
-    pass
+    if not __is_user_exist(user): return None
+    else: return __get_tickets(user)
         
 
 
 #__*****() ИЗВНЕ НЕ ВЫЗЫВАТЬ!!!
-def __is_user_exist(user):
+def __is_user_exist(user) -> bool:
     cursor.execute("SELECT TelegramUser FROM users WHERE TelegramUser = ?", (user,))
     return cursor.fetchone() is not None
 
-def __add_user(user, tickets):
+def __set_user(user, tickets):
     cursor.execute("INSERT INTO users (TelegramUser) VALUES (?)", (user,))
     db.commit()
-    add_tickets_to_user(user, tickets)
+    set_tickets_to_user(user, tickets)
+
+def __set_tickets(user, tickets):
+    cursor.execute("UPDATE users SET tickets = ? WHERE TelegramUser = ?", (tickets.strip(), user,))
+    db.commit()
+
+def __get_tickets(user) -> str :
+    cursor.execute("SELECT tickets FROM users WHERE TelegramUser = ?", (user,))
+    result = cursor.fetchone()[0]
+    return result
